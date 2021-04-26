6fba2791e101035933e48928a62b18721d377b91
Jack Hayes <hayesj6816@my.uwstout.edu>
2021-04-25 21:14:58 -0500

    Fix other references of window size

--- src/assets/PowerBoard.js
+++ src/assets/PowerBoard.js
@@ -87,7 +87,7 @@ animate();
 
 function init() {
 
-    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.01, 1e10);
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / (window.innerHeight - 200), 0.01, 1e10);
     camera.position.z = 6;
 
     controls = new THREE.OrbitControls(camera);
@@ -127,10 +127,10 @@ function init() {
 
 function onWindowResize() {
 
-    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.aspect = window.innerWidth / (window.innerHeight - 200);
     camera.updateProjectionMatrix();
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight - 200);
 
 }
 
